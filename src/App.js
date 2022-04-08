fed3e5f1236c20d902bdf2b02ea13078ebf4b588
Meike <meikehuettl@web.de>
2022-04-08 12:47:23 +0200

    new frontend setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, NavLink } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pokemonData, setPokemonData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/pokemon')
+        .then(response => response.json())
+        .then(data => setPokemonData(data))
+        .catch(error => console.log(error))
+    }, [])
+
+  return <div className='App'>
+    <nav>
+    <NavLink 
+    style={({ isActive }) => {
+      return {
+        color: isActive ? "#FCBF49" : "",
+        fontWeight: isActive ? "bold" : "",
+      };
+    }}
+    className="navLink"
+      to='/'>Home</NavLink>
+    <NavLink 
+    style={({ isActive }) => {
+      return {
+        color: isActive ? "#FCBF49" : "",
+        fontWeight: isActive ? "bold" : "",
+      };
+    }}
+    className="navLink" to='/pokemon'>Overview</NavLink>
+    </nav>
+    <main>
+    
+    
+    <Routes>
+      <Route exact path='/'  />
+      <Route exact path='/pokemon'  />
+      <Route exact path='/pokemon/:id'  />
+    </Routes>
+    </main>
+  </div>;
 }
 
 export default App;
