5959f02b596a00491075a3bd0d8920d2dc489f69
ChrisH <98359604+n8work@users.noreply.github.com>
2022-04-11 17:28:54 +0200

    pokemon card layout updated

--- src/components/PokemonDetailAnt.js
+++ src/components/PokemonDetailAnt.js
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
-import "./PokemonDetailAnt.css";
 
+import "./PokemonDetailAnt.css";
 import "antd/dist/antd.css";
 import { Progress, Card } from "antd";
 import {
-  EditOutlined,
-  EllipsisOutlined,
+  AppstoreOutlined,
   FireFilled,
   RadarChartOutlined,
-  SettingOutlined,
+  SyncOutlined,
 } from "@ant-design/icons";
 const { Meta } = Card;
 
@@ -17,64 +16,63 @@ const { Meta } = Card;
 
 export default function PokemonDetailAnt({ pokemonData }) {
   const params = useParams();
-  console.log("=> pokemonData: ", pokemonData);
+  /*   console.log("=> pokemonData: ", pokemonData);
   console.log("=> total number of pokemon: ", pokemonData.length);
-  console.log("=> current pokemon params.id: ", params.id);
+  console.log("=> current pokemon params.id: ", params.id); */
 
   const pokemon = pokemonData.find((pokemon) => params.id == pokemon.id);
-  console.log("=> Pokemon Name: ", pokemon.name.english);
+  // console.log("=> Pokemon Name: ", pokemon.name.english);
 
   // const imagePlaceholder = require("../static/contemplative-reptile.jpg");
   const cssCardWrapper = "cardWrapper";
 
-  const pokemonArtwork =
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-  console.log("=> pokemonArtwork: ", pokemonArtwork);
+  let pokemonName = pokemon.name.english;
+  let pokemonArtwork = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+  let pHP = pokemon.base.HP;
+  let pAttack = pokemon.base.Attack;
+  let pDefense = pokemon.base.Defense;
+  let pspAttack = pokemon.base["Sp. Attack"];
+  let pspDefense = pokemon.base["Sp. Defense"];
+  let pSpeed = pokemon.base.Speed;
 
   return (
     <>
       <div className={cssCardWrapper}>
         <Card
-          title={pokemon.name.english}
-          style={{ width: 440 }}
+          title={`#${pokemon.id} | ${pokemonName} Pokemon | ${pHP} HP`}
+          style={{ width: 500 }}
           cover={<img alt='pokemon' src={pokemonArtwork} />}
-          actions={[
-            <SettingOutlined key='setting' />,
-            <EditOutlined key='edit' />,
-            <EllipsisOutlined key='ellipsis' />,
-          ]}
         >
-          <Meta title="No. 114 Vine Pokemon | HT: 3'03 WT: 77.2 lbs" />
+          <Meta title={`HT: 3.03 | WT: 77.2 lbs`} />
           <br />
           <hr />
           <br />
           <Meta
             avatar={<FireFilled />}
-            title='Hydro Splash'
-            description={`Discard an Energy card to`}
+            title='ATTACK'
+            description={`Discard an Energy card attached to the defending Pokemon`}
           />
           <br />
-          <Progress type='circle' percent={40} width={60} />
+          <Progress type='circle' percent={pAttack} width={60} />
           <hr />
           <Meta
             avatar={<RadarChartOutlined />}
-            title='Poison Powder'
+            title='DEFENSE'
             description={`Your opponent's Active Pokemon is now Poisoned`}
           />
-          <br />
-          {/*           <Progress type='circle' percent={90} width={60} />
-           */}{" "}
-          <Progress percent={100} />
-          <br />
+
+          <Progress percent={pDefense} />
           <br />
           <hr />
+          <Meta avatar={<SyncOutlined spin />} title='SPEED' />
+
+          <Progress percent={pSpeed} steps={5} strokeColor='#52c41a' />
+          <hr />
           <Meta
-            avatar={<FireFilled />}
-            title='Flamethrower'
-            description={`Discard a Flamethrower Energy attached to this Pokemon`}
+            avatar={<AppstoreOutlined />}
+            title='SPECIALS'
+            description={`Special Attack: ${pspAttack} | Special Defense: ${pspDefense} `}
           />
-          <br />
-          <Progress type='circle' percent={60} width={60} />
         </Card>
         <br />
       </div>
